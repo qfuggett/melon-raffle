33c2153cc165bdec1071b286262797a0549aec16
qfuggett <qfuggett@gmail.com>
2021-03-11 09:00:24 +0000

    correctly import necessary files and update code to run program

--- raffle.py
+++ raffle.py
@@ -1,7 +1,6 @@
 """Read customer data from file and run a raffle."""
 
-from random import choice
-
+import random 
 
 class Customer(object):
     """A customer at Ubermelon."""
@@ -55,3 +54,6 @@ def run_raffle():
 
     customers = get_customers_from_file("customers.txt")
     pick_winner(customers)
+
+if __name__ == "__main__":
+    run_raffle()
--- version2/raffle.py
+++ version2/raffle.py
@@ -1,6 +1,30 @@
 """Randomly pick customer and print customer info"""
 
-# Add code starting here
-
 # Hint: remember to import any functions you need from
 # other files or libraries
+
+import random
+
+from customers import get_customers_from_file
+
+
+def random_winner(customers):
+    """Select a random winner from list of customers."""
+
+    random_customer = random.choice(customers)
+
+    name = random_customer.name
+    email = random_customer.email
+
+    print(f"Tell {name} at {email} that they've won")
+
+
+def raffle():
+    """Run the weekly raffle."""
+
+    customers = get_customers_from_file("customers.txt")
+    random_winner(customers)
+
+
+if __name__ == "__main__":
+    raffle()
